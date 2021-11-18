df55af8c254860e82b10665c076859e60ddb7270
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-11-19 02:37:41 +0300

    feat:add game artist section and footer

--- art-quiz/src/index.js
+++ art-quiz/src/index.js
@@ -1,7 +1,15 @@
 import addCategoriesPicture from './js/views/PictureCategories';
 import addCategoriesArtist from './js/views/ArtistCategories';
 import addNavigations from './js/views/Navigation';
-
+// import addQuestion from './js/views/ArtistQuiz'
 addCategoriesPicture();
 addCategoriesArtist();
 addNavigations();
+// addQuestion()
+
+const artistBlock = document.querySelector('.artist-inner-container');
+
+artistBlock.addEventListener('click', (e) => {
+  
+  e.target.classList.add('gray');
+});
