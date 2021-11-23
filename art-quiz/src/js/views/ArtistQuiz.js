04d7aed1d0764eec4e7e20540e44c9d74f438616
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-11-23 06:19:18 +0300

    feat:add artist Quiz  function

--- art-quiz/src/js/views/ArtistQuiz.js
+++ art-quiz/src/js/views/ArtistQuiz.js
@@ -1,14 +1,61 @@
-
+import images from "./images";
+const artistBlock = document.querySelector('.artist-inner-container');
+  const gameArtist = document.querySelector('.game-artist');
+  const options = document.querySelectorAll('.answer-var'); // поля с ответами
+const dots = artistBlock.querySelectorAll('.dot');
+let dotsArray = []; // 
 export default function artistQuizPlay() {
-  const artistBlock = document.querySelector('.artist-inner-container');
-  const gameArtist = document.querySelector('.game-artist')
+  
+let currentQuestion = 0;
+let countQuestions = 0;
 
 artistBlock.addEventListener('click', (e) => {
   
   e.target.classList.add('gray');
   artistBlock.classList.toggle("hide");
   gameArtist.classList.toggle("hide");
+
 });
+
+options.forEach((el) => {
+  el.addEventListener('click', () => {
+
+      console.log(el.textContent);
+
+      if (countQuestions < 10) {
+          const result = el.textContent === images[ currentQuestion ].author;
+          // окрашиваем фон ответа в зависимости от ответа (зеленый/красный)
+          result ? el.style.backgroundColor = 'green' : el.style.backgroundColor = 'red';
+          dotsArray.push(result);
+          showDotsGame1();
+      }
+
+      currentQuestion++;
+      countQuestions++;
+      if (countQuestions < 10) {
+          showQuestion(currentQuestion);
+          console.log(dotsArray);
+      }
+      if (countQuestions === 10) {
+       
+          // showResultPopUp();
+          const indexArray = (currentQuestion - 10) / 10;
+          scores.result[ indexArray ] = dotsArray;
+          // scores.saveLS();
+      }
+  });
+});
+
   
 }
-
+function showDotsGame1() {
+  // красим все точки в серый
+  for (let i = 0; i < dots.length; i++) {
+      dots[ i ].style.backgroundColor = "gray";
+  }
+  // красим в красный ложные ответы и в зеленый правильные
+  for (let i = 0; i < dotsArray.length; i++) {
+      console.log(dotsArray.length);
+      dotsArray[ i ] ? dots[ i ].style.backgroundColor = "green" : dots[ i ].style.backgroundColor = "red";
+  }
+}
--- art-quiz/src/js/views/PictureQuiz.js
+++ art-quiz/src/js/views/PictureQuiz.js
@@ -1,7 +1,9 @@
-
+import images from './images';
 export default function pictureQuizPlay() {
   const pictureBlock = document.querySelector('.picture-inner-container');
   const gamePicture = document.querySelector('.game-pictures')
+  const options = document.querySelectorAll('.answer-var'); // поля с ответами
+const dots = pictureBlock.querySelectorAll('.dot'); // поля с ответами
 
 pictureBlock.addEventListener('click', (e) => {
   
